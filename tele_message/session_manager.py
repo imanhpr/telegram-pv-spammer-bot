4c7b687ab3a0e364faa7e17bee9ff1ac94ea016b
Iman Hosseini Pour <imanhpr1999@gmail.com>
2022-04-04 12:40:24 +0430

    feat(tele_message.session_manager): Add session_manager

---
+++ tele_message/session_manager.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
+from pyrogram import Client, idle
+
+SESSION_DIR: Path = Path(".").joinpath("number_sessions")
+
+
+@dataclass(frozen=True)
+class SessionAgent:
+    phone_number: int
+    country_code: int
+    api_id: int
+    api_hash: str
+    sms_rule: bool = False
+    _cl: Optional[Client] = None
+
+    @property
+    def full_number(self) -> str:
+        return str(self.country_code) + str(self.phone_number)
+
+    def client(self) -> Client:
+        if self._cl:
+            return self._cl
+        cl = Client(
+            session_name=self.full_number,
+            phone_number=self.full_number,
+            api_hash=self.api_hash,
+            api_id=self.api_id,
+            force_sms=self.sms_rule,
+            workdir=SESSION_DIR,
+        )
+        cl.start()
+        cl.stop()
+        self._cl = cl
+        return cl
