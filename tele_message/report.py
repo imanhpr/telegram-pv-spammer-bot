b4ea53aef486248a373af9e44fdebbbb3da39add
Iman Hosseini Pour <imanhpr1999@gmail.com>
2022-04-06 08:09:56 +0430

    docs(tele_message.report): Add csv report writer

--- tele_message/report.py
+++ tele_message/report.py
@@ -1,5 +1,8 @@
+import csv
 from dataclasses import dataclass, field
 from datetime import datetime
+from pathlib import Path
+from typing import Iterable
 
 
 @dataclass(frozen=True)
@@ -13,3 +16,15 @@ class ReportNameGenerator:
             name=self.name,
             date=datetime.now().replace(microsecond=0).isoformat(),
         )
+
+
+def write_csv_report(
+    file_name: str, fieldnames: list[str], data: Iterable[dict]
+) -> tuple[str, str]:
+    name = ReportNameGenerator(file_name)
+    path = Path(name.full_name)
+    with path.open("w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+    return name.full_name, path.absolute()
