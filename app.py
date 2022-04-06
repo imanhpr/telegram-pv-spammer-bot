8e5bb0bd33b408a66df11ef2a55288bcbe954b0a
Iman Hosseini Pour <imanhpr1999@gmail.com>
2022-04-06 14:04:29 +0430

    chore(Project): Add LICENSE

--- app.py
+++ app.py
@@ -25,12 +25,34 @@ from tele_message.utils import csv_len, make_sessions, users_csv_reader
 
 console = Console()
 
+LICENSE_MESSAGE = """\nMIT License
+
+Copyright (c) 2022 [bold #af00ff]Iman Hosseini Pour[/bold #af00ff]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.\n"""
+
 
 class MainMenuItems(Enum):
     SEND_TO_ALL = "Send message to all"
     MAKE_NEW_SESSIONS = "Make New Sessions"
     GENERATE_BASE_FOLDER = "Make Default Data Folder"
-
+    License = "License"
     EXIT = "Exit"
 
 
@@ -164,3 +186,5 @@ if __name__ == "__main__":
                     "We didn't have any problem and all messages have sent successfully"
                 )
                 console.log("Back To Main Menu ...")
+        elif answer == MainMenuItems.License:
+            console.log(LICENSE_MESSAGE)
