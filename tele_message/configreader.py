746b115693b599aa2e61a5b7c5d6ccc69da277a6
Iman Hosseini Pour <imanhpr1999@gmail.com>
2022-04-06 13:42:10 +0430

    feat(tele_message.configreader): Add defualt_config maker and fix a bug

--- tele_message/configreader.py
+++ tele_message/configreader.py
@@ -1,13 +1,32 @@
 import configparser
 from pathlib import Path
 
-CONFIG_DIR = Path(".").joinpath("config", "SpamBot.ini")
+
+def make_defualt_config() -> None:
+    if not CONFIG_DIR.exists():
+        CONFIG_DIR.mkdir()
+    config = configparser.ConfigParser()
+    config["FILE"] = {
+        "root_dir": Path(".", "Data").absolute(),
+        "users_file": "users.csv",
+        "phones_file": "phone.csv",
+    }
+    with CONFIG_FILE.open("w") as configfile:
+        config.write(configfile)
+
+
+CONFIG_DIR = Path(".").joinpath("config")
+CONFIG_FILE = CONFIG_DIR.joinpath("SpamBot.ini")
+
+if not CONFIG_FILE.exists():
+    make_defualt_config()
+
 MAIN_CONFIG = configparser.ConfigParser()
-MAIN_CONFIG.read(CONFIG_DIR)
+MAIN_CONFIG.read(CONFIG_FILE)
 USER_DATA_DIR_NAME = "user_data"
 ROOT_DIR: Path = Path(MAIN_CONFIG.get("FILE", "root_dir"))
 USERS_FILE_NAME: str = MAIN_CONFIG.get("FILE", "users_file")
-USERS_FILE_PATH: Path(
+USERS_FILE_PATH: Path = Path(
     ROOT_DIR.joinpath(USER_DATA_DIR_NAME).joinpath(USERS_FILE_NAME),
 )
 PHONE_FILE_NAME: str = MAIN_CONFIG.get("FILE", "phones_file")
